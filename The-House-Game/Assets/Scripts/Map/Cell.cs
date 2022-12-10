61dd2b582588b19bf1d5cc53267da1f4d913f06d
Igor <ia2002Zakharov@gmail.com>
2022-12-10 20:07:57 +0300

    add-basic-classes-for-map-room-and-cell

---
+++ The-House-Game/Assets/Scripts/Map/Cell.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cell : MonoBehaviour
+{
+    [SerializeField] private float cellSize;
+
+    private float positionX;
+    private float positionY;
+
+    [SerializeField] private int id;
+
+    void Start()
+    {
+        id = -1;
+        positionX = transform.position.x;
+        positionY = transform.position.y;
+    }
+
+    public void SetId(int _id) {
+        id = _id;
+    }
+
+    public float GetPositionX() 
+    {
+        return positionX;
+    }
+
+    public float GetPositionY() 
+    {
+        return positionY;
+    }
+
+    public float GetCellSize() 
+    {
+        return cellSize;
+    }
+}
