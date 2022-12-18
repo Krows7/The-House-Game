821697cf4e60fe064e61b05e499fd3de7d069f17
Konstantin Shaga <kvshaga@edu.hse.ru>
2022-12-18 17:04:18 +0300

    Implemented Leader Skills
    * Use Skills via "UseSkill()"
    * RatLeader Prefab Contains Test Settings (Fix in Future)

--- The-House-Game/Assets/Scripts/Characters/Leader.cs
+++ The-House-Game/Assets/Scripts/Characters/Leader.cs
@@ -6,6 +6,38 @@ namespace Units.Settings
 {
     public class Leader : Army
     {
+        public float skillTeleportDelay;
+        public int skillInfluence;
+        public float skillSuccessChance;
+        float skillTimer = -1;
 
+        public void UseSkill()
+        {
+            skillTimer = 0;
+            SetVisible(false);
+            CurrentCell.DellUnit();
+        }
+
+        void Update()
+        {
+            if (skillTimer == -1) return;
+            if(skillTimer >= skillTeleportDelay)
+            {
+                if(Random.insideUnitCircle.x <= skillSuccessChance)
+                {
+                    fraction.Influence += skillInfluence;
+                }
+                CurrentCell.SetUnit(this);
+                SetVisible(true);
+                skillTimer = -1;
+                return;
+            }
+            skillTimer += Time.deltaTime;
+        }
+
+        private void SetVisible(bool Visible)
+        {
+            transform.GetChild(0).GetComponent<MeshRenderer>().enabled = Visible;
+        }
     }
 }
