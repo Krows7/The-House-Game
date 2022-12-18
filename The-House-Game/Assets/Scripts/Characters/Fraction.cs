9d9f62306e61aa3300b65e702ff2ddef21284077
Konstantin Shaga <kvshaga@edu.hse.ru>
2022-12-18 17:12:35 +0300

    Moved Spawn Bases into Inspector
    * Set for Each Fraction Their Spawn Rooms

--- The-House-Game/Assets/Scripts/Characters/Fraction.cs
+++ The-House-Game/Assets/Scripts/Characters/Fraction.cs
@@ -11,17 +11,17 @@ namespace Units.Settings
 
         public int Influence = 0;
         public GameObject[] units;
-        public string type;
+        public Room spawnRoom;
 
         void Start()
         {
             foreach (var unit in units) unit.GetComponent<Unit>().fraction = this;
-            Spawn(type, units);
+            Spawn(units);
         }
 
-        private void Spawn(string baseName, params GameObject[] units)
+        private void Spawn(params GameObject[] units)
         {
-            var cells = GameObject.Find("/TemporaryDebugObjects/TemporaryFixedMap/Map/Spawn" + baseName).transform;
+            var cells = spawnRoom.transform;
             var range = Enumerable.Range(0, cells.childCount).ToList().OrderBy(a => Random.Range(0, int.MaxValue)).ToList();
             for (int i = 0; i < units.Length; i++)
             {
